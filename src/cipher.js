1096d0ad8290352868c757c21a9cfff9d6c838f2
Javiera.H <Javierab.herrera@gmail.com>
2021-05-13 15:40:17 +0200

    Creación de función offset

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,44 @@
-const cipher = {
-  // ...
-};
+function cipher() {
+  let traeTexto = document.getElementById("boxMens");
+  let valorTexto = traeTexto.value;
+  let codigoGuardado="";
+  let offset= parseInt(document.getElementById("numOffset").value);
+  let ascii;
+  let palabraFinal="";
+  if (codeMode==1){
+    offset= -offset;
+  }
+console.log(codeMode);
+  for(let j=0; j< valorTexto.length; j++){
+    valorLetra=valorTexto.charCodeAt(j);
+    ascii=valorLetra;
+    console.log("valorletra: "+ valorLetra);
+    if ( 97<=valorLetra && valorLetra <= 122){
+      ascii= valorLetra + offset; 
+      if (ascii > 122) { 
+        ascii = valorLetra -26 +  offset %26;
+      }
+      if (ascii < 97) {
+        ascii= valorLetra +26 +  offset %26;
+      }
+    }
+    
+    if ( 65<= valorLetra && valorLetra <=90){
+      ascii= valorLetra + offset; 
+      if (ascii > 90) { 
+        ascii = valorLetra -26 +  offset %26;
+      }
+      if (ascii < 65) {
+        ascii= valorLetra +26 +  offset %26;
+      }
+    }
+    
+    codigoGuardado= String.fromCharCode(ascii);
+    palabraFinal= palabraFinal + codigoGuardado;
 
-export default cipher;
+  }
+
+  document.getElementById("boxMenCodi").value= palabraFinal;
+}
+ 
+

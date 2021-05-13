d740ea3a1b98884ae3fe6903b086348d93115d23
Javiera.H <Javierab.herrera@gmail.com>
2021-05-13 19:55:15 +0200

    Cambios en valores internos de botones

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 let codeMode=0;
 function Codecero(){
     codeMode=0;
+    document.getElementById("btnCodi").innerHTML= "Codificar"
+
 }
 function Codeuno(){
     codeMode=1;
+    document.getElementById("btnCodi").innerHTML= "Decodificar"
 }
 
 

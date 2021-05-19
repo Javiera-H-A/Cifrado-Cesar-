49284c199c8b9868cac2ea97bbf7834619d5fe0d
Javiera.H <Javierab.herrera@gmail.com>
2021-05-18 22:15:13 -0400

    Creando  Objeto y Funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,84 @@
-function cipher() {
-  let traeTexto = document.getElementById("boxMens");
-  let valorTexto = traeTexto.value;
-  let codigoGuardado="";
-  let offset= parseInt(document.getElementById("numOffset").value);
-  let ascii;
-  let palabraFinal="";
-  if (codeMode==1){
-    offset= -offset;
-  }
-console.log(codeMode);
-  for(let j=0; j< valorTexto.length; j++){
-    valorLetra=valorTexto.charCodeAt(j);
-    ascii=valorLetra;
-    console.log("valorletra: "+ valorLetra);
-    if ( 97<=valorLetra && valorLetra <= 122){
-      ascii= valorLetra + offset; 
-      if (ascii > 122) { 
-        ascii = valorLetra -26 +  offset %26;
-      }
-      if (ascii < 97) {
-        ascii= valorLetra +26 +  offset %26;
-      }
-    }
-    
-    if ( 65<= valorLetra && valorLetra <=90){
-      ascii= valorLetra + offset; 
-      if (ascii > 90) { 
-        ascii = valorLetra -26 +  offset %26;
-      }
-      if (ascii < 65) {
-        ascii= valorLetra +26 +  offset %26;
-      }
-    }
-    
-    codigoGuardado= String.fromCharCode(ascii);
-    palabraFinal= palabraFinal + codigoGuardado;
+ const cipher = { 
+  encode: (offset, valorTexto) => {
 
-  }
-
-  document.getElementById("boxMenCodi").value= palabraFinal;
+    if (typeof offset !== "number") {
+      throw new TypeError ('${ offset } es distinto de {"number"}')
 }
- 
+    if (typeof valorTexto!== "string") {
+      throw new TypeError ('${ valorTexto } es distinto de {"string"}')
+}
+    let codigoGuardado="";
+    let ascii;
+    let palabraFinal="";
+    let valorLetra="";
+    
+    for(let j=0; j< valorTexto.length; j++){
+      valorLetra=valorTexto.charCodeAt(j);
+      ascii=valorLetra;
+      if ( 97<=valorLetra && valorLetra <= 122){
+        ascii= valorLetra + offset %26; 
+        if (ascii > 122) { 
+          ascii = ascii -26;
+          
+        }
+        else if (ascii < 97) {
+          ascii= ascii +26;
+        }
+      }
+      if ( 65<= valorLetra && valorLetra <=90){
+        ascii= valorLetra + offset %26; 
 
+        if (ascii > 90) { 
+          ascii = ascii -26;
+        }
+        else if (ascii < 65) {
+          ascii= ascii +26;
+        }
+      }
+      codigoGuardado= String.fromCharCode(ascii);
+      palabraFinal+= codigoGuardado;
+    }
+    return palabraFinal;
+  },
+  decode: (offset, valorTexto) => {
+
+    if (typeof offset !== "number") {
+      throw new TypeError ('${ offset } es distinto de {"number"}')
+}
+    if (typeof valorTexto!== "string") {
+      throw new TypeError ('${ valorTexto } es distinto de {"string"}')
+}
+    let codigoGuardado="";
+    let ascii;
+    let palabraFinal="";
+    let valorLetra="";
+    offset=-offset;
+    
+    for(let j=0; j< valorTexto.length; j++){
+      valorLetra=valorTexto.charCodeAt(j);
+      ascii=valorLetra;
+      if ( 97<=valorLetra && valorLetra <= 122){
+        ascii= valorLetra + offset %26; 
+        if (ascii > 122) { 
+          ascii = ascii -26;
+        }
+        else if (ascii < 97) {
+          ascii= ascii +26;
+        }
+      }
+      if ( 65<= valorLetra && valorLetra <=90){
+        ascii= valorLetra + offset %26; 
+        if (ascii > 90) { 
+          ascii = ascii -26;
+        }
+        else if (ascii < 65) {
+          ascii= ascii +26;
+        }
+      }
+      codigoGuardado= String.fromCharCode(ascii);
+      palabraFinal+= codigoGuardado;
+    }
+    return palabraFinal;
+  } 
+};
+export default cipher;
